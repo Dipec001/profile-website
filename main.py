41b077f91ce733697c851a014fe8b0e79c09fd7f
dipec <dpecchukwu@gmail.com>
2023-06-12 21:44:02 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, url_for, flash, request, abort, session
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
