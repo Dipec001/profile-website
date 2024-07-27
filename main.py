3412c0bc3297e8ab833cea29d2e26a979c50a54e
Dipec001 <dpecchukwu@gmail.com>
2024-07-27 09:36:35 +0100

    changed Debug to True

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash, request, abort, session
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -17,4 +17,4 @@ def projects():
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
